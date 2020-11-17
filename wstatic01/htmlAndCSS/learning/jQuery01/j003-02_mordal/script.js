883539a3a24b28edbdb369c158b7cf11316b9025
kuba-zaru <kubazaru@gmail.com>
2020-11-17 23:25:34 +0900

    html css javascript 新規登録

---
+++ wstatic01/htmlAndCSS/learning/jQuery01/j003-02_mordal/script.js
@@ -0,0 +1,11 @@
+  
+-$(function() {
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  $('#submit-btn').click(function() {
+    $('#login-modal').fadeOut();
+  });
+
+});
---
+++ wstatic01/htmlAndCSS/learning/jQuery01/j003_mordal/script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+
+  $('#submit-btn').click(function() {
+    $('#login-modal').fadeOut();
+  });
+
+  // 「.signup-show」のclickイベントを作成してください
+
+});
