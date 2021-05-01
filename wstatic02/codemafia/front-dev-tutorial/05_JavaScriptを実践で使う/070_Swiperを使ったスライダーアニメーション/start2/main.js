6464f01f58f49fe6a25e25b127b3a1908ea414a8
kuba-zaru <kubazaru@gmail.com>
2021-05-02 01:44:19 +0900

    HTML&CSS 最新化

---
+++ wstatic02/codemafia/front-dev-tutorial/05_JavaScriptを実践で使う/070_Swiperを使ったスライダーアニメーション/start2/main.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal", // スライド方向を指定
+  loop: true, // ループ
+  effect: "slide",
+  //   effect: "flip",
+  //   effect: "coverflow",
+  //   effect: "cube",
+  //   effect: "fade",
+  slidesPerView: 1.2, //スライドを2つ（分）表示
+
+  spaceBetween: 10,
+  centeredSlides: true, // true を指定するとアクティブなスライドを中央に配置
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets", //ページネーションの種類
+    clickable: true, //クリックに反応させる
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
