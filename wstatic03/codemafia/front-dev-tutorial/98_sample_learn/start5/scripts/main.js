aac280c5a1f33c55921812fca1f8f0176b2b4925
kuba-zaru <kubazaru@gmail.com>
2021-05-05 17:31:42 +0900

    HTML&CSS header menu まで

--- wstatic03/codemafia/front-dev-tutorial/98_sample_learn/start5/scripts/main.js
+++ wstatic03/codemafia/front-dev-tutorial/98_sample_learn/start5/scripts/main.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const so = new ScrollObserver(".tween-animate-title", cb);
 
+  // スライド画像のアニメーション用
+  // cover-slide
   const _inviewAnimation = function (el, inview) {
     if (inview) {
       el.classList.add("inview");
@@ -19,4 +21,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
   const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
+
+  // ヘッダーの背景色用
+  // nav-trigger
+  const header = document.querySelector(".header");
+  const _navAnimation = function (el, inview) {
+    if (inview) {
+      console.log("_navAnimation 1");
+      header.classList.remove("triggered");
+    } else {
+      console.log("_navAnimation 2");
+      header.classList.add("triggered");
+    }
+  };
+  const so3 = new ScrollObserver(".nav-trigger", _navAnimation, {
+    once: false,
+  });
+
+  new MobileMenu();
 });
