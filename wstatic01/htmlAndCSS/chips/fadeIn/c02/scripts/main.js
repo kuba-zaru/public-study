6325ec41bb6a17457504fbca083374bf65ae75ed
kuba-zaru <kubazaru@gmail.com>
2021-05-09 18:53:18 +0900

    HTML & CSS フェードイン モジュール化

---
+++ wstatic01/htmlAndCSS/chips/fadeIn/c02/scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btn01 = document.querySelector(".btn01");
+  btn01.addEventListener(judgeClickEventType(), () =>
+    toggleClass(".item_container", "inview")
+  );
+
+  // const btn02 = document.querySelector(".btn02");
+  // btn02.addEventListener(judgeClickEventType(), () =>
+  //   toggleClass(".item_container2", "inview")
+  // );
+
+  // const btn03 = document.querySelector(".btn03");
+  // btn03.addEventListener(judgeClickEventType(), () =>
+  //   toggleClass(".item_container3", "inview")
+  // );
+});
