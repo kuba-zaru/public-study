023a4c359bcf982df495685538b5938732e64dd5
kuba-zaru <kubazaru@gmail.com>
2021-05-10 19:59:34 +0900

    HTML & CSS hero slider

---
+++ wstatic01/htmlAndCSS/chips/hero/c02/scripts/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // const btn01 = document.querySelector(".btn01");
+  // btn01.addEventListener(judgeClickEventType(), () =>
+  //   toggleClass(".item_container", "inview")
+  // );
+
+  const heroSlider = new HeroSlider();
+});
