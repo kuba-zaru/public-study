9c8bddc04935fbfe96f25be3929849450d27917a
kuba-zaru <kubazaru@gmail.com>
2021-05-20 21:25:13 +0900

    HTML & CSS メニュー

---
+++ wstatic01/htmlAndCSS/chips/menu/c09/scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // mobile menu button
+  const mmBtn = document.querySelector(".mobile-menu__button");
+  mmBtn.addEventListener(judgeClickEventType(), () =>
+    toggleClass("#container", "menu-open")
+  );
+  mmBtn.addEventListener(judgeClickEventType(), () =>
+    toggleClass(".mobile-menu", "inview")
+  );
+
+  // menu cover
+  const mmCover = document.querySelector(".mobile-menu__cover");
+  mmCover.addEventListener(judgeClickEventType(), () =>
+    toggleClass("#container", "menu-open")
+  );
+  mmCover.addEventListener(judgeClickEventType(), () =>
+    toggleClass(".mobile-menu", "inview")
+  );
+
+  // menu link
+  const mmLinks = document.querySelectorAll(".mobile-menu__link");
+  mmLinks.forEach((item) => {
+    item.addEventListener(judgeClickEventType(), () =>
+      toggleClass("#container", "menu-open")
+    );
+    item.addEventListener(judgeClickEventType(), () =>
+      toggleClass(".mobile-menu", "inview")
+    );
+  });
+});
