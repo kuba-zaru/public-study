9aac95733d71bdd5257158a7940858d6ff43e6bc
kuba-zaru <kubazaru@gmail.com>
2021-05-23 15:08:23 +0900

    HTML & CSS loader

---
+++ wstatic01/htmlAndCSS/chips/loader/c01/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // const btn01 = document.querySelector(".btn01");
+  // btn01.addEventListener(judgeClickEventType(), () =>
+  //   toggleClass(".item_container", "inview")
+  // );
+});
