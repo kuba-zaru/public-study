b50c67f3d7d31dda8b7f7c5cf731316e2622d2b6
kuba-zaru <kubazaru@gmail.com>
2021-05-23 11:19:47 +0900

    HTML & CSS fadein 最新化

--- wstatic01/htmlAndCSS/chips/fadeIn/c03/scripts/main.js
+++ wstatic01/htmlAndCSS/chips/fadeIn/c03/scripts/main.js
@@ -1,21 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn01 = document.querySelector(".btn01");
-  btn01.addEventListener(judgeClickEventType(), () =>
-    toggleClass(".item_container", "inview")
-  );
-
   const observers = [];
   observers.push(new ScrollObserver(".appear", inviewAnimation));
-
-  // const btn02 = document.querySelector(".btn02");
-  // btn02.addEventListener(judgeClickEventType(), () =>
-  //   toggleClass(".item_container2", "inview")
-  // );
-
-  // const btn03 = document.querySelector(".btn03");
-  // btn03.addEventListener(judgeClickEventType(), () =>
-  //   toggleClass(".item_container3", "inview")
-  // );
 });
 
 function inviewAnimation(el, inview) {
