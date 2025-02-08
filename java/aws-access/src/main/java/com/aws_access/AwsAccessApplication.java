ce858109d1a74b828875f15a94d2dd3ddac08b24
kuba-zaru <kubazaru@gmail.com>
2025-02-08 21:20:51 +0900

    ホットリロードの設定を入れた

--- java/aws-access/src/main/java/com/aws_access/AwsAccessApplication.java
+++ java/aws-access/src/main/java/com/aws_access/AwsAccessApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsAccessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsAccessApplication.class, args);
-		System.out.println("AWS Resource Access Sample へようこそ。");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwsAccessApplication.class, args);
+        System.out.println("AWS Resource Access Sample へようこそ。");
+    }
 }
--- java/aws-access/src/test/java/com/aws_access/AwsAccessApplicationTests.java
+++ java/aws-access/src/test/java/com/aws_access/AwsAccessApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AwsAccessApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
