739ccd8004374b506d94d5da90cb10e59410c55b
kuba-zaru <kubazaru@gmail.com>
2025-02-09 17:15:24 +0900

    HelloをHTMLテンプレートをレイアウトに対応させる。

--- java/aws-access/src/main/java/com/aws_access/hello/HelloController.java
+++ java/aws-access/src/main/java/com/aws_access/hello/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    /**
+     * ハローワールド画面を表示する
+     *
+     * @return
+     */
     @GetMapping("/hello")
     public String hello() {
         return "hello/hello";
